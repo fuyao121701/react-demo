8a16906081a3b39dea3c10bdfdc804c808e6485e
fuyao <fuy@polysoft.com.cn>
2018-11-23 15:51:57 +0800

    设置rem单位并完成组件化

---
+++ src/component/Footer/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import FooterData from '../../../public/FooterData.json';
+import './index.less'
+
+export default class Footer extends React.Component {
+     constructor(props){
+         super(props);
+         this.state = {
+             FooterData:[
+                 {
+                     img:require('../../image/footer/index.png'),
+                     text:'首页'
+                 },
+                 {
+                     img:require('../../image/footer/index.png'),
+                     text:'首页'
+                 },
+                 {
+                     img:require('../../image/footer/index.png'),
+                     text:'首页'
+                 },
+                 {
+                     img:require('../../image/footer/index.png'),
+                     text:'首页'
+                 },
+             ]
+         }
+
+     }
+
+     render() {
+         let FooterData  = this.state.FooterData;
+         console.log(FooterData);
+         return(
+             <ul className="footerBox">
+                 {
+                     FooterData.map((item,index)=>{
+                         return(
+                             <li key={index}>
+                                 <img src={item.img} alt=""/>
+                                 <p>{item.text}</p>
+                             </li>
+                         )
+                     })
+                 }
+             </ul>
+         )
+
+
+
+
+
+     }
+
+ }
---
+++ src/component/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ class Header extends React.Component {
+     constructor(props){
+         super(props);
+     }
+     render() {
+         return <div>我是头部</div>
+     }
+
+ }
+
+export default Header;
